5a09d0786afc68b66b41618ac9ba71e35f3b0fcf
Stas Petrov <stas_petrov_01@mail.ru>
2021-08-09 11:07:55 +0500

    yandex metrika

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,7 +8,22 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	@auth <meta name="api_token" content="{{ Auth::user()->api_token }}"> @endauth
 	@if(App::environment('production'))
-		<meta name="yandex-verification" content="09cdfabf53172981" />
+		{{-- <meta name="yandex-verification" content="09cdfabf53172981" /> --}}
+		<!-- Yandex.Metrika counter -->
+		<script type="text/javascript" >
+			(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+			m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+		
+			ym(83810059, "init", {
+				clickmap:true,
+				trackLinks:true,
+				accurateTrackBounce:true,
+				webvisor:true
+			});
+		</script>
+		<noscript><div><img src="https://mc.yandex.ru/watch/83810059" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+		<!-- /Yandex.Metrika counter -->
 		<!-- Google Tag Manager -->
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
